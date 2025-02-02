b6951ba44beece68e435f24e3432fe87731d3dfc
arya-map22 <arya.prayoga222.ap@gmail.com>
2025-02-02 19:36:43 +0700

    Add dummy data and card logic

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-import { ProgressbarComponent } from '../progressbar/progressbar.component';
+import { Component, computed, signal } from '@angular/core';
+import { Card } from './card.interface';
 
 @Component({
   selector: 'app-card',
-  imports: [ProgressbarComponent],
+  imports: [],
+  providers: [],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
-export class CardComponent {}
+export class CardComponent {
+  cards = dummyData;
+
+  currentIndex = signal(0);
+  card = computed(() => this.cards[this.currentIndex()]);
+  showAnswer = signal(false);
+
+  onNext() {
+    if (this.currentIndex() + 1 < this.cards.length) {
+      this.currentIndex.update((prev) => prev + 1);
+      this.showAnswer.set(false);
+    }
+  }
+
+  onPrevious() {
+    if (this.currentIndex() - 1 >= 0) {
+      this.currentIndex.update((prev) => prev - 1);
+      this.showAnswer.set(false);
+    }
+  }
+
+  onToggleAnswer() {
+    this.showAnswer.update((prev) => !prev);
+  }
+}
+
+const dummyData: Card[] = [
+  {
+    id: 'c1',
+    question: 'What is the difference between Angular and React?',
+    answer:
+      'Angular is a full-fledged framework and React is just a UI library',
+  },
+  {
+    id: 'c2',
+    question: 'What is the benefit of using Typescript?',
+    answer: 'Typechecking so that bug can be caught at development',
+  },
+];
